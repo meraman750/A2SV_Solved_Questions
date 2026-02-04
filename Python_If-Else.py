e239a9ce134b2e3c4e1d1b2b5c729d1c796520d3
Meheretabe Abayneh <meraman675@gmail.com>
2026-02-04 21:46:18 +0300

    Remove shebang line
    Removed shebang line from Python_If-Else.py

---
+++ Python_If-Else.py
@@ -0,0 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2 == 1 or 6 <= n <= 20:
+        print("Weird")
+    elif 2 <= n <= 5 or n > 20:
+        print("Not Weird")
+        
