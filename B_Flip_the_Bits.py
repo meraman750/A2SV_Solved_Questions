015dbf6debbe31c7484497234f65e7e911f89407
Meheretabe Abayneh <meraman675@gmail.com>
2026-03-02 21:47:51 +0300

    Solved Problems

--- B_Flip_the_Bits.py
+++ B_Flip_the_Bits.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import Counter
 
 def list_(): return list(map(int, input().split()))
 def str_(): return list(map(str, input().split()))
@@ -9,22 +9,25 @@ for _ in range(t):
     k = int(input())
     one = input()
     two = input()
-    
-    mp = defaultdict(int)
-    flag = True
-
-    for i in range(k):
-        if one[i] != two[i]:
-            mp[one[i]] +=1
+    def flip(x): return "0" if x =="1" else "1"
+    mp = Counter(one)
+    flag = False
+    for i in range(k-1, -1, -1):
+        if flag:
+            temp = flip(one[i])
         else:
-            if mp["0"] != mp["1"]:
-                flag = False
-                break
-            else:
-                mp["0"] = 0
-                mp["1"] = 0
+            temp = one[i]
 
-    if mp["0"] != mp["1"]:
-        flag=False
-    
-    print("YES" if flag else "NO")
+        if temp != two[i]:
+            if mp["0"] != mp["1"]:
+                print("NO")
+                break
+            if flag:
+                flag = False
+            else:
+                flag = True
+        # if flag:
+        #     x = flip(one[i])
+        mp[one[i]]-=1 
+    else:
+        print("YES")
--- D_Phoenix_and_Socks.py
+++ D_Phoenix_and_Socks.py
@@ -10,25 +10,38 @@ for _ in range(t):
     arr = list_()
     left = arr[:l]
     right = arr[l:]
-    left.sort()
-    right.sort()
     
     count_left = Counter(left)
     count_right = Counter(right)
-    ans = 0
-    print(count_right)
-    print(count_left)
+    
+    for key, val in count_left.items():
+        x = min(val, count_right[key])
+        count_left[key]-= x
+        count_right[key]-= x
+        l-=x
+        r-=x
 
     if l == r:
-        for key, val in count_right.items():
-            if key not in count_left:
-                ans+=1
-
-        print(ans)
+        print(l)
         continue
-    # else:
-    #     if len(left) > len(right):
-    #         for key, val in count_left.items():
-    #             if key not in count_right:
-    #                 ans+= 
+
+    if l > r:
+        count_left, count_right = count_right, count_left
+        l, r = r, l
+        
+    need = (r-l)//2
+    ans = 0
+    for key, val in count_right.items():
+        while val > 1 and need > 0:
+            # print(key, val, need)
+            count_right[key]-=2
+            val-=2
+            need-=1
+            ans+=1
+
+    ans += (need * 2)
+    ans += l
+
+    print(ans)
+
 
