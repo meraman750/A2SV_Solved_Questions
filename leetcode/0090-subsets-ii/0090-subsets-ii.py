1f6e927df7eb86de2287f67b0eb37e717474a8fa
Meheretabe Abayneh <meraman675@gmail.com>
2026-03-31 18:43:21 +0300

    Add solution for Subsets II

--- leetcode/0090-subsets-ii/0090-subsets-ii.py
+++ leetcode/0090-subsets-ii/0090-subsets-ii.py
@@ -1,17 +1,16 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+        ans = set()
         temp = []
 
         def calc(n):
-            if sorted(temp) not in ans:
-                ans.append(sorted(temp[:])) 
+            # if sorted(temp) not in ans:
+            ans.add(tuple(sorted((temp[:]))))
 
             for i in range(n, len(nums)):
                 temp.append(nums[i])
-                # ans.append(temp)
                 calc(i+1)
                 temp.pop()
 
         calc(0)
-        return ans
+        return list(ans)
