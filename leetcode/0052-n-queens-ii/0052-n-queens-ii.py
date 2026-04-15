5463ad8ec2e9ead31dc0ce4579863b3bbe7f6a00
Meheretabe Abayneh <meraman675@gmail.com>
2026-04-15 18:31:32 +0300

    Add solution for N-Queens II

---
+++ leetcode/0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        col = set()
+        p_dig = set()
+        n_dig = set()
+        ans = []
+        temp = [["."] * n for i in range(n)]
+        
+        def is_valid(i, j):
+            if j in col or i+j in p_dig or i-j in n_dig:
+                return False
+            return True   
+
+        def calc(row):
+            if row == n:
+                t = ["".join(r) for r in temp]
+                ans.append(t)
+                return 
+
+            for c in range(n):
+                if is_valid(row, c):
+                    col.add(c)
+                    p_dig.add(row+c)
+                    n_dig.add(row-c)
+                    temp[row][c] = "Q"
+                    
+                    calc(row+1)
+                    
+                    col.remove(c)
+                    p_dig.remove(row+c)
+                    n_dig.remove(row-c)
+                    temp[row][c] = "."
+
+        calc(0)
+        return len(ans)
