5824ba3e190497fcdc4a9c65a54c5192fe5bd1b7
Meheretabe Abayneh <meraman675@gmail.com>
2026-04-19 14:46:06 +0300

    Time: 503 ms (6.75%), Space: 29.9 MB (6.95%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
@@ -1,36 +1,29 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
+        ans = []
+        res = []
+        def calc(n):
+            if n >= len(pattern):
+                res.append("".join(map(str, ans[:])))
+                return 
 
-        n = len(pattern)
-        used = [False] * 10
-        ans = ""
+            if pattern[n] == "I":
+                for i in range(ans[-1]+1, 10):
+                    if i not in set(ans):
+                        ans.append(i)
+                        calc(n+1)
+                        ans.pop()
+            else:
+                for i in range(1, ans[-1]):
+                    if i not in set(ans):
+                        ans.append(i)
+                        calc(n+1)
+                        ans.pop()
 
-        def backtrack(path):
-            nonlocal ans
-
-            if ans:
-                return
-
-            if len(path) == n + 1:
-                ans = path
-                return
-
-            for d in range(1, 10):
-
-                if used[d]:
-                    continue
-
-                if path:
-                    prev = int(path[-1])
-
-                    if pattern[len(path)-1] == 'I' and d <= prev:
-                        continue
-                    if pattern[len(path)-1] == 'D' and d >= prev:
-                        continue
-
-                used[d] = True
-                backtrack(path + str(d))
-                used[d] = False
-
-        backtrack("")
-        return ans
+        for i in range(1, 10):
+            ans.append(i)
+            calc(0)
+            ans.pop()
+        res.sort()
+        return res[0]
+        
