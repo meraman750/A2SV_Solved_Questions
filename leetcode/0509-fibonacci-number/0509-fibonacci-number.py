3cc102ed1d8ced31e0e666f828fe7852e8c78f23
Meheretabe Abayneh <meraman675@gmail.com>
2026-05-19 16:44:26 +0300

    Add solution for Fibonacci Number

--- leetcode/0509-fibonacci-number/0509-fibonacci-number.py
+++ leetcode/0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,12 +1,19 @@
 class Solution:
+    memo = {}
     def fib(self, n: int) -> int:
+        if n == 1 or n== 0:
+            return n
+        if n not in self.memo:
+            self.memo[n] = self.fib(n-1) + self.fib(n-2)
+
+        return self.memo[n]
         
-        def calc(n):
-            if n == 1:
-                return 1
-            if n == 0:
-                return 0
+        # def calc(n):
+        #     if n == 1:
+        #         return 1
+        #     if n == 0:
+        #         return 0
             
-            return calc(n-2) + calc(n-1)
+        #     return calc(n-2) + calc(n-1)
         
-        return calc(n)
+        # return calc(n)
